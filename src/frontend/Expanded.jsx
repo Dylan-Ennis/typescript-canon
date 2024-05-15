3b0b35a509011044231c1380e22e1b47b990ec53
ambscara <150319615+ambscara@users.noreply.github.com>
2024-05-14 21:13:39 -0700

    updated expanded

---
+++ src/frontend/Expanded.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from 'react-bootstrap/Button'
+import App from '../App';
+
+function Expand() {
+    const [isExpanded, setIsExpanded] = useState(false);
+  
+    function handleClick() {
+      setIsExpanded(!isExpanded);
+    }
+  
+    return (
+      <App>
+        <div>
+          <p>
+            blah blah blah 
+            {isExpanded && (
+              <span>
+                filler text here. need to imput more text.filler text here. need to imput more text.filler text here. need to imput more text.filler text here. need to imput more text.filler text here. need to imput more text.
+                filler text here. need to imput more text.filler text here. need to imput more text.filler text here. need to imput more text.filler text here. need to imput more text.
+              </span>
+            )}
+          </p>
+          {!isExpanded && (
+            <Button variant="primary" onClick={handleClick}>Read more</Button>
+          )}
+        </div>
+      </App>
+    );
+  }
+  
+  export default Expand;
+  
