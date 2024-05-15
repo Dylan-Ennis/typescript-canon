a2cd5f3ae8ccb18bf656e61f97141dfed9d77ade
Dylan Ennis <dennis@ucsd.edu>
2024-05-14 19:55:26 -0700

    added CRUD routes for story

--- backend/controllers/stories.js
+++ backend/controllers/stories.js
@@ -14,7 +14,22 @@ router.get('/', async (req, res) => {
     }
 });
 
-// POST request to create a new story
+// Get by id
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const story = await Stories.findById(id);
+        if (!story) {
+            return res.status(404).json({ message: 'Story not found' });
+        }
+        res.json(story);
+    } catch (error) {
+        console.error('Error finding story by ID:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+// Create
 router.post("/", async (req, res) => {
     try {
         const newStory = await Stories.create(req.body); // Create a new story
@@ -22,6 +37,42 @@ router.post("/", async (req, res) => {
     } catch (error) {
         console.log("Error creating story:", error);
         res.status(500).json({ message: "Error creating story" });
+    }
+});
+
+
+// Delete by id
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedStory = await Stories.findByIdAndDelete(id);
+        if (!deletedStory) {
+            return res.status(404).json({ message: 'Story not found' });
+        }
+        res.json({ message: 'Story deleted successfully', deletedStory });
+    } catch (error) {
+        console.error('Error deleting story:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+// UPDATE by id
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, author, storyContent } = req.body;
+    try {
+        const updatedStory = await Stories.findByIdAndUpdate(id, {
+            title,
+            author,
+            storyContent
+        }, { new: true });
+        if (!updatedStory) {
+            return res.status(404).json({ message: 'Story not found' });
+        }
+        res.json({ message: 'Story updated successfully', updatedStory });
+    } catch (error) {
+        console.error('Error updating story:', error);
+        res.status(500).json({ message: 'Internal server error' });
     }
 });
 
