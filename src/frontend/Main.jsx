487959528cb79c3c7e3ac46b93e6d03a39f70c82
Travis Jones <toj817@gmail.com>
2024-05-14 21:34:07 -0700

    Replaced some fillers in the Mainjsx

--- src/frontend/Main.jsx
+++ src/frontend/Main.jsx
@@ -4,16 +4,14 @@ import App from '../App';
 
 function Main({stories}) {
   return(
-
   <App>
     <div className='story' style= {{color:"black",backgroundColor:"#897e5d"}}>
-      <p>Tup tup tup tup tup tup tup tup tup tup tup tup tup tup tup tup tup</p>
+      <p>{stories.storycontent}</p>
     </div>
     <nav style={{color:"#897e5d",}}>Info Log:
     <ul>
-            <li>Info Log:</li>
-            <li>Original Title: Tup</li>
-            <li>Original Author: Tup202116</li>
+            <li>Original Title: {stories.Title}</li>
+            <li>Original Author: {stories.Author}</li>
             <li>Word Count: 500</li>
             <li>Canon Continuations: 10</li>
             <li>Fanon Continuations: 30</li>
